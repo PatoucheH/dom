357910308c9b34fa3e69ed2d2aad910a8569ca48
Hugo Pat <Hugo.Paternoster@hotmail.com>
2025-03-17 16:02:34 +0100

    correction bug exercice 4.2

--- 4.navigate/script.js
+++ 4.navigate/script.js
@@ -9,13 +9,20 @@ liste.insertBefore(lastChild, firstChild);
 
 const main = document.querySelector("main");
 const mainChilds = main.children;
+
 const secondChild = mainChilds[1];
 const thirdChild = mainChilds[2];
+
 const secondTitle = secondChild.querySelector("h2");
 const thirdTitle = thirdChild.querySelector("h2");
 
-secondChild.appendChild(thirdTitle);
+const secondSectP = secondChild.querySelector("p");
+const thirdSectP = thirdChild.querySelector("p");
+
+secondChild.insertBefore(thirdTitle, secondSectP);
 thirdChild.appendChild(secondTitle);
+thirdChild.appendChild(thirdSectP);
+console.log(thirdChild);
 
 // third exercise
 
